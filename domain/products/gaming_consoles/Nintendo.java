7558007536cd499b2236890b9b9fa3ef098b3f29
Alex M <48068999+KronkPTL@users.noreply.github.com>
2020-04-07 18:18:43 +0300

    - Deleted unnecessary directories; - Created a better file and directory hierarchy.

--- domain/products/gaming_consoles/Nintendo.java
+++
@@ -1,7 +0,0 @@
-package domain.products.gaming_consoles;
-
-public final class Nintendo extends ConsoleType {
-    public Nintendo(Double price, String producer, String originCountry, String productionYear) {
-        super(price, producer, originCountry, "Gaming console", "Nintendo", productionYear);
-    }
-}
--- products/gaming_consoles/Nintendo.java
+++
@@ -1,9 +0,0 @@
-package products.gaming_consoles;
-
-public final class Nintendo extends ConsoleType
-{
-    public Nintendo(Double price, String producer, String originCountry, String productionYear)
-    {
-        super(price, producer, originCountry, "Gaming console", "Nintendo", productionYear);
-    }
-}
